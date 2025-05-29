5e6751919a1ef8a47a9dc42b945c94317daf225f
Julien Barbe <julien.barbe@me.com>
2025-05-29 14:20:14 +0200

    Bump version: 0.5.0 → 0.5.1

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../src/niamoto"))
 project = "niamoto"
 copyright = "2025, Julien Barbe"
 author = "Julien Barbe"
-release = "0.5.0"
+release = "0.5.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
