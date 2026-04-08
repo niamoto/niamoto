187bbc2435cae2343943a93ac9b1fbf78113cd30
Julien Barbe <julien.barbe@me.com>
2026-04-08 15:34:57 +0200

    feat: improve publish workspace and desktop window state

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -381,6 +381,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .manage(ServerState {
             process: Mutex::new(None),
         })
